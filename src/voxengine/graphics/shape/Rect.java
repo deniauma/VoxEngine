ce5361e4d76b20aa42c5c69ec2fa2096c6346c1b
deniauma <mathieu.deniaud@gmail.com>
2015-10-26 23:20:08 +0100

    Added basicRenderer

---
+++ src/voxengine/graphics/shape/Rect.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package voxengine.graphics.shape;
+
+/**
+ *
+ * @author Adminmatt
+ */
+public class Rect {
+    
+    public float x;
+    public float y;
+    public float width;
+    public float height;
+    
+    public Rect() {
+        
+    }
+}
